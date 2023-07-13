c577b26018e08c88dc9c366fd393cbc69600fc2c
Dimas <dima991999@mail.ru>
2023-07-13 20:59:19 +0300

    added TestSuites Service, requests, models, tests;

---
+++ BussinesObject/Api/Services/TestSuitesService.cs
@@ -0,0 +1,72 @@
+﻿using BussinesObject.Api.RestCore.Clients;
+using BussinesObject.Api.RestEntities.TestSuiteModels;
+using Core.Api;
+using Newtonsoft.Json;
+using RestSharp;
+
+namespace BussinesObject.Api.Services;
+
+public class TestSuitesService : BaseService
+{
+    private static readonly int projectId = Core.Settings.Settings.API.MainProjectId;
+    private static readonly string TestSuitesEndpoint = "test-suites";
+    private static readonly string TestSuiteByIdEndpoint = "test-suites/{testSuiteId}";
+    private static readonly string TestSuiteRestoreEndpoint = "test-suite/{testSuiteId}/restore";
+
+    public TestSuitesService() : base(new TestMonitorClient())
+    {
+    }
+
+    public RestResponse GetAllTestSuites()
+    {
+        var request = new RestRequest(TestSuitesEndpoint, Method.Get)
+            .AddQueryParameter("project_id", projectId);
+
+        return ApiClient.Execute(request);
+    }
+
+    public RestResponse GetTestSuite(int testSuiteId)
+    {
+        var request = new RestRequest(TestSuiteByIdEndpoint, Method.Get)
+            .AddUrlSegment("testSuiteId", testSuiteId);
+
+        return ApiClient.Execute(request);
+    }
+
+    public RestResponse DeleteTestSuite(int testSuiteId)
+    {
+        var request = new RestRequest(TestSuiteByIdEndpoint, Method.Delete)
+            .AddUrlSegment("testSuiteId", testSuiteId);
+
+        return ApiClient.Execute(request);
+    }
+
+    public RestResponse RestoreTestSuite(int testSuiteId)
+    {
+        var request = new RestRequest(TestSuiteRestoreEndpoint, Method.Post)
+            .AddUrlSegment("testSuiteId", testSuiteId);
+
+        return ApiClient.Execute(request);
+    }
+
+    public RestResponse CreateTestSuite(TestSuiteRequestModel model)
+    {
+        var request = new RestRequest(TestSuitesEndpoint, Method.Post);
+
+        var body = JsonConvert.SerializeObject(model);
+        request.AddBody(body);
+
+        return ApiClient.Execute(request);
+    }
+
+    public RestResponse UpdateTestSuite(int testSuiteId, TestSuiteRequestModel model)
+    {
+        var request = new RestRequest(TestSuiteByIdEndpoint, Method.Put)
+            .AddUrlSegment("testSuiteId", testSuiteId);
+
+        var body = JsonConvert.SerializeObject(model);
+        request.AddBody(body);
+
+        return ApiClient.Execute(request);
+    }
+}
---
+++ BussinesObject/Api/Steps/TestSuitesRequest.cs
@@ -0,0 +1,62 @@
+﻿using BussinesObject.Api.RestEntities;
+using BussinesObject.Api.RestEntities.TestSuiteModels;
+using BussinesObject.Api.Services;
+using BussinesObject.Api.Utils;
+using Newtonsoft.Json;
+using NUnit.Framework;
+
+namespace BussinesObject.Api.Steps;
+
+public static class TestSuitesRequest
+{
+    public static TestSuitesService Service { get; } = new();
+
+    public static TestSuiteResponseModel RestoreTestSuite(int testSuiteId)
+    {
+        var respon = Service.RestoreTestSuite(testSuiteId);
+        RequestHelper.CheckResponse(respon, false, System.Net.HttpStatusCode.OK);
+
+        var responModel = JsonConvert
+            .DeserializeObject<CommonResponse<TestSuiteResponseModel>>(respon.Content!)?.Data;
+        Assert.Multiple(() =>
+        {
+            Assert.That(responModel, Is.Not.Null);
+            Assert.That(responModel?.Id, Is.EqualTo(testSuiteId));
+        });
+
+        return responModel!;
+    }
+
+    public static TestSuiteResponseModel CreateTestSuite(TestSuiteRequestModel model)
+    {
+        var respon = Service.CreateTestSuite(model);
+        RequestHelper.CheckResponse(respon, false, System.Net.HttpStatusCode.Created);
+
+        var responModel = JsonConvert
+            .DeserializeObject<CommonResponse<TestSuiteResponseModel>>(respon.Content!)?.Data;
+        Assert.Multiple(() =>
+        {
+            Assert.That(responModel, Is.Not.Null);
+            Assert.That(responModel?.Name, Is.EqualTo(model.Name));
+            Assert.That(responModel?.ProjectId, Is.EqualTo(model.ProjectId));
+        });
+
+        return responModel!;
+    }
+
+    public static TestSuiteResponseModel UpdateTestSuite(int testSuiteId, TestSuiteRequestModel model)
+    {
+        var respon = Service.UpdateTestSuite(testSuiteId, model);
+        RequestHelper.CheckResponse(respon);
+
+        var responModel = JsonConvert
+            .DeserializeObject<CommonResponse<TestSuiteResponseModel>>(respon.Content!)?.Data;
+        Assert.Multiple(() =>
+        {
+            Assert.That(responModel, Is.Not.Null);
+            Assert.That(responModel?.Id, Is.EqualTo(testSuiteId));
+        });
+
+        return responModel!;
+    }
+}
