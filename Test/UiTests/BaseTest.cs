2ac771f351aa6ad01803dd545bdbbf2ef573c449
Dimas <dima991999@mail.ru>
2023-07-28 15:33:31 +0300

    added TimeOutTearDown

--- Test/UiTests/BaseTest.cs
+++ Test/UiTests/BaseTest.cs
@@ -10,6 +10,10 @@ public class BaseTest
     [TearDown]
     public virtual void TearDown()
     {
+        Core.LogSession.CurrentSession
+            .Debug($"TearDown timeout {Core.Settings.Settings.Browser.TimeOutTearDown} sec.");
+        Task.Delay(Core.Settings.Settings.Browser.TimeOutTearDown * 1_000).Wait();
+
         if (TestContext.CurrentContext.Result.Outcome.Status == NUnit.Framework.Interfaces.TestStatus.Failed)
         {
             Browser.Instance.TakeScreenShot("ScreenShotFail");
